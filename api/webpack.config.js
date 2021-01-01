36813d55e0c3ef76295ca1b68e88b76f9af18ef7
Jason Loo <loojason@amazon.com>
2020-12-31 17:46:19 -0800

    Update jest, babel and node-notify dependencies

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         test: /\.js$/,
         loader: "babel-loader",
         include: __dirname,
-        exclude: /node_modules/,
+        exclude: /node_modules/
       }
     ],
   }
